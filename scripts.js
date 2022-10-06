979867ad43315e84744356b96ebccacb1971e76e
pennyflipper.dev <76460018+SaanSaelee@users.noreply.github.com>
2022-10-05 21:32:51 -0700

    added parallax scrolling

--- scripts.js
+++ scripts.js
@@ -1,41 +1,57 @@
 const skills = document.querySelectorAll(".skills-title, .skills-box");
 const projects = document.querySelectorAll(".projects-title, .project");
 const schools = document.querySelectorAll(
-  ".education-title, .school, .certs-title, .certs"
+    ".education-title, .school, .certs-title, .certs"
 );
 const contacts = document.querySelectorAll(
-  ".profile-pic, .contact-title, .contact-details"
+    ".profile-pic, .contact-title, .contact-details"
 );
 
 const options = {
-  root: null,
-  threshold: 0.4,
-  rootMargin: "0px",
+    root: null,
+    threshold: 0.4,
+    rootMargin: "0px",
 };
 
 const observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return;
-    }
-    console.log(entry);
-    entry.target.classList.toggle("fade-in");
-    observer.unobserve(entry.target);
-  });
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+            return;
+        }
+        console.log(entry);
+        entry.target.classList.toggle("fade-in");
+        observer.unobserve(entry.target);
+    });
 }, options);
 
 skills.forEach((skill) => {
-  observer.observe(skill);
+    observer.observe(skill);
 });
 
 projects.forEach((project) => {
-  observer.observe(project);
+    observer.observe(project);
 });
 
 schools.forEach((school) => {
-  observer.observe(school);
+    observer.observe(school);
 });
 
 contacts.forEach((contact) => {
-  observer.observe(contact);
+    observer.observe(contact);
+});
+
+// Parallax scrolling
+
+let stars = document.getElementById("stars");
+let moon = document.getElementById("moon");
+let mountains_behind = document.getElementById("mountains_behind");
+let heroTitle = document.getElementById("hero__title");
+let mountains_front = document.getElementById("mountains_front");
+
+window.addEventListener("scroll", () => {
+    let value = window.scrollY;
+    stars.style.left = value * 0.25 + "px";
+    moon.style.top = value * 0.85 + "px";
+    mountains_behind.style.top = value * 0.5 + "px";
+    heroTitle.style.marginRight = value * 2 + "px";
 });
